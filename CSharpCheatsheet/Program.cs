9e97bfe35e08227869e1cfc3d8175c12769f4328
Jenniferrmuniz <Jennifer.r.muniz@gmail.com>
2019-11-17 21:56:43 -0500

    first commit

---
+++ CSharpCheatsheet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpCheatsheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("This writes to the console on its own line");
+            Console.Write("Type hello here : ");
+            string word = Console.ReadLine();
+            Console.Write(word);
+
+        }
+    }
+}
